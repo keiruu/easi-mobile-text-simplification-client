c42c0f1e5dfb8250a03235001e416917703cb59e
Abigail Kaye Unating <abbyunat@gmail.com>
2022-06-01 15:19:27 +0800

    Fixed layout for text simplification

--- easi/lib/text_simplification.dart
+++ easi/lib/text_simplification.dart
@@ -43,6 +43,17 @@ class _TextSimplificationState extends State<TextSimplification> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Image.asset(
+          'assets/logo.png',
+          height: 50,
+          width: 50,
+        ),
+        centerTitle: true,
+      ),
       body: loading
           ? const Center(
               child: Padding(
@@ -51,89 +62,69 @@ class _TextSimplificationState extends State<TextSimplification> {
                     indicatorType: Indicator.ballPulse,
                   )))
           : Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.all(15),
-                    child: TextField(
-                      controller: inputController,
-                      maxLines: 10,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: 'Enter text to simplify'),
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(15, 0, 15, 0), 
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    InkWell(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        alignment: Alignment.centerLeft,
+                        child: Icon(Icons.arrow_back_rounded, color: Color(0xFF5274AE), size: 36,),
+                      ),
                     ),
-                  ),
-                  // Padding(
-                  //   padding: EdgeInsets.all(15),
-                  //   child: TextField(
-                  //     minLines: 1,
-                  //     maxLines: 100,
-                  //     enabled: false,
-                  //     decoration: InputDecoration(
-                  //       border: OutlineInputBorder(),
-                  //       hintText: '$simplified',
-                  //     ),
-                  //   ),
-                  // ),
-                  FractionallySizedBox(
-                    widthFactor: 1,
-                    child: Container(
-                        height: 200,
-                        margin: const EdgeInsets.all(15.0),
-                        padding: const EdgeInsets.all(10.0),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                              color: Color.fromARGB(255, 111, 111, 111),
-                              width: 0.8,
-                            ),
-                            borderRadius: BorderRadius.circular(3)),
-                        child: Text(
-                          '$simplified',
-                          style: TextStyle(fontSize: 16.0),
-                        )),
-                  ),
-                  Padding(
+                    Padding(
                       padding: EdgeInsets.all(15),
-                      child: TextButton(
-                        style: ButtonStyle(
-                          foregroundColor:
-                              MaterialStateProperty.all<Color>(Colors.white),
-                          backgroundColor:
-                              MaterialStateProperty.all<Color>(Colors.blue),
-                          padding: MaterialStateProperty.all(
-                              const EdgeInsets.fromLTRB(138, 13, 138, 13)),
-                        ),
-                        onPressed: () {
-                          setPrompt();
-                          setSimplifiedText(prompt);
-                        },
-                        child: Text('Simplify'),
-                      )),
-                  Padding(
-                      padding: EdgeInsets.all(2),
-                      child: TextButton(
-                        style: ButtonStyle(
-                          foregroundColor:
-                              MaterialStateProperty.all<Color>(Colors.white),
-                          backgroundColor:
-                              MaterialStateProperty.all<Color>(Colors.blue),
-                          padding: MaterialStateProperty.all(
-                              const EdgeInsets.fromLTRB(138, 13, 138, 13)),
-                        ),
-                        onPressed: () {
-                          // CODE TO PUSH TO A NEW SCREEN
-                          // Navigator.of(context).push(
-                          //   MaterialPageRoute(
-                          //     builder: (context) =>
-                          //       Camera(),
-                          //   ),
-                          // );
-                        },
-                        child: Text('Camera'),
-                      )),
-                ],
-              ),
+                      child: TextField(
+                        controller: inputController,
+                        maxLines: 10,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Enter text you want to understand better.'),
+                      ),
+                    ),
+                    FractionallySizedBox(
+                      widthFactor: 1,
+                      child: Container(
+                          height: 200,
+                          margin: const EdgeInsets.all(15.0),
+                          padding: const EdgeInsets.all(10.0),
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Color.fromARGB(255, 111, 111, 111),
+                                width: 0.8,
+                              ),
+                              borderRadius: BorderRadius.circular(3)),
+                          child: Text(
+                            '$simplified',
+                            style: TextStyle(fontSize: 16.0),
+                          )),
+                    ),
+                    Padding(
+                        padding: EdgeInsets.all(15),
+                        child: TextButton(
+                          style: ButtonStyle(
+                            foregroundColor:
+                                MaterialStateProperty.all<Color>(Color(0xFF5274AE)),
+                            backgroundColor:
+                                MaterialStateProperty.all<Color>(Color(0xFFCDDBF2)),
+                            padding: MaterialStateProperty.all(
+                                const EdgeInsets.fromLTRB(123, 25, 123, 25)),
+                          ),
+                          onPressed: () {
+                            setPrompt();
+                            setSimplifiedText(prompt);
+                          },
+                          child: Text('Simplify'),
+                        )
+                      ),
+                  ],
+                ),
+                )
             ),
     );
   }
