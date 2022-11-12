17a50ffaa21f7837b78ef4f9d737b108648049c9
Abigail Kaye Unating <abbyunat@gmail.com>
2022-11-12 16:28:11 +0800

    Added connectivity checker

--- easi/lib/internet_not_available.dart
+++ easi/lib/internet_not_available.dart
@@ -8,20 +8,37 @@ class InternetNotAvailable extends StatelessWidget {
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
 
-    return Stack(
+    return Material(
+      child: Stack(
       children: [
         Container(
           height: mediaQuery.size.height,
           width: MediaQuery.of(context).size.width,
           color: Colors.white,
           child: Center(
-            child: Text(
-                  'No Internet ConneADJHASKJHDJKSAction!!!',
-                  style: TextStyle(color: Colors.black, fontSize: 20),
+            child: Column(
+              children: [
+                Padding(padding: EdgeInsets.only(top: 200),
+                  child: Image.asset(
+                    'assets/no_internet.png',
+                    height: 235,
+                    width: 235,
+                  ),
                 ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 20, 0, 5),
+                  child: Text("Whoops!", style: TextStyle(fontSize: 20, color: Colors.black, fontFamily: 'Inter', fontWeight: FontWeight.w500),),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 40),
+                  child: Text("Slow or no internet connection, please check your internet settings", textAlign: TextAlign.center, style: TextStyle(fontSize: 14, color: Colors.black, fontFamily: 'Inter', fontWeight: FontWeight.normal))
+                )
+              ],
+            ),
           ),
         )
       ],
+    ),
     );
   }
 }
